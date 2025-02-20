10a79dcf5d33fb826648521e75b4b5fb8897982c
Debora Nogueira <deboran.silva@hotmail.com>
2025-02-19 21:52:27 -0300

    chore: salva alterações antes de atualizar o Expo

---
+++ conexao_bd/db/config/config.js
@@ -0,0 +1,24 @@
+
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": '@Nxero10anos',
+    "database": "vegconnect",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": '@Nxero10anos',
+    "database": "vegconnect",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": '@Nxero10anos',
+    "database": "vegconnect",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
